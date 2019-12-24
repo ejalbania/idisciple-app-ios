b3d40de2c8a53578b4b6f48b4c4e3232140175f2
ejalbania <emmanuel.albania@novare.com.hk>
2019-12-24 15:50:01 +0800

    Fixed issues in schedule

--- iDiscipleApp/iDiscipleApp/Community Tab/CommunityViewController.swift
+++ iDiscipleApp/iDiscipleApp/Community Tab/CommunityViewController.swift
@@ -12,162 +12,126 @@ import SwiftyJSON
 
 class CommunityViewController: ButtonBarPagerTabStripViewController {
 
-    var communityView: CommunityView!
-    
-    var familyGroupArray : [FamilyGroup] = []
-    var profileArray : [Profile] = []
-    
-    let groupVC = GroupsViewController(itemInfo: "GROUPS")
-    let delegatesVC = DelegatesViewController(itemInfo: "DELEGATES")
-    
-    override func viewDidLoad() {
-        
-        settings.style.buttonBarBackgroundColor = .white
-        settings.style.buttonBarItemBackgroundColor = .white
-        settings.style.selectedBarBackgroundColor = .black
-        settings.style.buttonBarItemFont = UIFont(name: "Montserrat-Bold", size: 24)!//.boldSystemFont(ofSize: 14)
-        settings.style.selectedBarHeight = 0.0
-        settings.style.buttonBarMinimumLineSpacing = 0
-        settings.style.buttonBarItemTitleColor = .black
-        settings.style.buttonBarItemsShouldFillAvailableWidth = true
-        settings.style.buttonBarLeftContentInset = 0
-        settings.style.buttonBarRightContentInset = 0
-        
-        changeCurrentIndexProgressive = {(oldCell: ButtonBarViewCell?, newCell: ButtonBarViewCell?, progressPercentage: CGFloat, changeCurrentIndex: Bool, animated: Bool) -> Void in
-            guard changeCurrentIndex == true else { return }
-            oldCell?.label.textColor = .lightGray
-            newCell?.label.textColor = .black
-        }
-        
-        communityView = CommunityView(frame: CGRect.zero)
-        self.view.addSubview(communityView)
-        
-        //communityView.scrollView.contentInsetAdjustmentBehavior = .never
-        
-        self.buttonBarView = communityView.buttonBarView
-        self.containerView = communityView.scrollView
-        
-        self.view.setNeedsLayout()
-        // AutoLayout
-        communityView.autoPinEdgesToSuperviewEdges(with: UIEdgeInsets.zero)
-        
-//        DispatchQueue.main.async {
-//            self.loadCommunityData()
-//
-//            self.groupVC.familyGroupArray = self.familyGroupArray
-//            self.groupVC.profileArray = self.profileArray
-//            self.delegatesVC.familyGroupArray = self.familyGroupArray
-//            self.delegatesVC.profileArray = self.profileArray
-//        }
-        
-        loadCommunityData()
-        
-        groupVC.familyGroupArray = familyGroupArray
-        groupVC.profileArray = profileArray
-        delegatesVC.familyGroupArray = familyGroupArray
-        delegatesVC.profileArray = profileArray
-        
-        super.viewDidLoad()
+  var communityView: CommunityView!
 
-    }
-    
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        
-        self.view.setNeedsLayout()
-        //self.view.layoutIfNeeded()
-    }
-    
-    func adjustViews(){
-        DispatchQueue.main.async {
-            self.view.setNeedsLayout()
-        }
-    }
-    
-    /*
-     // MARK: - Navigation
-     
-     // In a storyboard-based application, you will often want to do a little preparation before navigation
-     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-     // Get the new view controller using segue.destination.
-     // Pass the selected object to the new view controller.
-     }
-     */
-    
-    // MARK: - PagerTabStripDataSource
-    
-    override func viewControllers(for pagerTabStripController: PagerTabStripViewController) -> [UIViewController] {
-        return [groupVC, delegatesVC]
-    }
-    
-    func loadCommunityData(){
-        
-        familyGroupArray.removeAll()
-        
-        let filename = "family_groups"
-        
-        //Check if family_groups.json exists
-        let testRetrieve = JSONCache.getOptional(filename, as: JSON.self)
-        if((testRetrieve) != nil){
-            //load
-            let jsonValue = JSON(testRetrieve!)
-            //debugPrint("\(jsonValue)")
-            for (_, subJson) in jsonValue[0]["data"]{
-                
-                self.familyGroupArray.append(FamilyGroup(familyGroupID: subJson["fg_id"].intValue,
-                                                                    familyGroupName: subJson["fg_name"].stringValue,
-                                                                    familyGroupLeadID: subJson["fg_lead_id"].intValue))
-            }
-            
-            //reloadTables
-            //self.groupsView.familyGroupTableView.reloadData()
-            //            updateFamilyGroupCounter()
-            //            enableFamilyGroupController()
-            loadProfiles()
-            
-        }else{
-            debugPrint("\(filename) not found!")
-        }
-        
-    }
-    
-    func loadProfiles(){
-        
-        profileArray.removeAll()
-        let filename = "profile"
-        
-        //Check if schedule.json exists
-        let testRetrieve = JSONCache.getOptional(filename, as: JSON.self)
-        if((testRetrieve) != nil){
-            //load
-            let jsonValue = JSON(testRetrieve!)
-            //debugPrint("\(jsonValue)")
-            for (_, subJson) in jsonValue[0]["data"]{
-                
-                self.profileArray.append(Profile(profileID: subJson["id"].intValue,
-                                                            profileName: subJson["name"].stringValue,
-                                                            firstName: subJson["firstname"].stringValue,
-                                                            middleName: subJson["middlename"].stringValue,
-                                                            lastName: subJson["lastname"].stringValue,
-                                                            nickName: subJson["nickname"].stringValue,
-                                                            gender: subJson["gender"].stringValue,
-                                                            country: subJson["country"].stringValue,
-                                                            familyGroupID: subJson["fg_id"].intValue,
-                                                            firstWorkshop: subJson["workshop_number_1"].intValue,
-                                                            secondWorkshop: subJson["workshop_number_2"].intValue,
-                                                            imagePath: subJson["img_path"].stringValue,
-                                                            imageName: subJson["img_name"].stringValue))
-            }
-            
-            //reloadTables
-            //self.groupsView.familyGroupTableView.reloadData()
-            //updateFamilyGroupCounter()
-            //enableFamilyGroupController()
-            
-        }else{
-            debugPrint("\(filename) not found!")
-        }
-        
+  var familyGroupArray : [FamilyGroup] = []
+  var profileArray : [Profile] = []
+
+  let groupVC = GroupsViewController(itemInfo: "GROUPS")
+  let delegatesVC = DelegatesViewController(itemInfo: "DELEGATES")
+
+  override func viewDidLoad() {
+
+    settings.style.buttonBarBackgroundColor = .white
+    settings.style.buttonBarItemBackgroundColor = .white
+    settings.style.selectedBarBackgroundColor = .black
+    settings.style.buttonBarItemFont = UIFont(name: "Montserrat-Bold", size: 24)!//.boldSystemFont(ofSize: 14)
+    settings.style.selectedBarHeight = 0.0
+    settings.style.buttonBarMinimumLineSpacing = 0
+    settings.style.buttonBarItemTitleColor = .black
+    settings.style.buttonBarItemsShouldFillAvailableWidth = true
+    settings.style.buttonBarLeftContentInset = 0
+    settings.style.buttonBarRightContentInset = 0
+
+    changeCurrentIndexProgressive = {(oldCell: ButtonBarViewCell?, newCell: ButtonBarViewCell?, progressPercentage: CGFloat, changeCurrentIndex: Bool, animated: Bool) -> Void in
+      guard changeCurrentIndex == true else { return }
+      oldCell?.label.textColor = .lightGray
+      newCell?.label.textColor = .black
     }
 
+    communityView = CommunityView(frame: CGRect.zero)
+    self.view.addSubview(communityView)
+
+    self.buttonBarView = communityView.buttonBarView
+    self.containerView = communityView.scrollView
+
+    self.view.setNeedsLayout()
+    // AutoLayout
+    communityView.autoPinEdgesToSuperviewEdges(with: UIEdgeInsets.zero)
+
+    loadCommunityData()
+
+    groupVC.familyGroupArray = familyGroupArray
+    groupVC.profileArray = profileArray
+    delegatesVC.familyGroupArray = familyGroupArray
+    delegatesVC.profileArray = profileArray
+
+    super.viewDidLoad()
+
+  }
+
+  override func viewWillAppear(_ animated: Bool) {
+    super.viewWillAppear(animated)
+
+    self.view.setNeedsLayout()
+    //self.view.layoutIfNeeded()
+  }
+
+  func adjustViews(){
+    DispatchQueue.main.async {
+      self.view.setNeedsLayout()
+    }
+  }
+
+  override func viewControllers(for pagerTabStripController: PagerTabStripViewController) -> [UIViewController] {
+    return [groupVC, delegatesVC]
+  }
+
+  func loadCommunityData(){
+
+    familyGroupArray.removeAll()
+
+    let filename = "family_groups"
+
+    //Check if family_groups.json exists
+    let testRetrieve = JSONCache.getOptional(filename, as: JSON.self)
+    if((testRetrieve) != nil){
+      //load
+      let jsonValue = JSON(testRetrieve!)
+      //debugPrint("\(jsonValue)")
+      for (_, subJson) in jsonValue[0]["data"]{
+
+        self.familyGroupArray.append(FamilyGroup(familyGroupID: subJson["fg_id"].intValue,
+                                                 familyGroupName: subJson["fg_name"].stringValue,
+                                                 familyGroupLeadID: subJson["fg_lead_id"].intValue))
+      }
+      loadProfiles()
+
+    }else{
+      debugPrint("\(filename) not found!")
+    }
+
+  }
+
+  func loadProfiles(){
+
+    profileArray.removeAll()
+    let filename = "profile"
+
+    //Check if schedule.json exists
+    let testRetrieve = JSONCache.getOptional(filename, as: JSON.self)
+    if((testRetrieve) != nil){
+      //load
+      let jsonValue = JSON(testRetrieve!)
+      //debugPrint("\(jsonValue)")
+      for (_, subJson) in jsonValue[0]["data"]{
+
+        self.profileArray.append(Profile(profileID: subJson["id"].intValue,
+                                         profileName: subJson["name"].stringValue,
+                                         firstName: subJson["firstname"].stringValue,
+                                         middleName: subJson["middlename"].stringValue,
+                                         lastName: subJson["lastname"].stringValue,
+                                         nickName: subJson["nickname"].stringValue,
+                                         gender: subJson["gender"].stringValue,
+                                         country: subJson["country"].stringValue,
+                                         familyGroupID: subJson["fg_id"].intValue,
+                                         firstWorkshop: subJson["workshop_number_1"].intValue,
+                                         secondWorkshop: subJson["workshop_number_2"].intValue,
+                                         imagePath: subJson["img_path"].stringValue,
+                                         imageName: subJson["img_name"].stringValue))
+      }
+    }else{
+      debugPrint("\(filename) not found!")
+    }
+  }
 }
--- iDiscipleApp/iDiscipleApp/Schedule Tab/ScheduleViewController.swift
+++ iDiscipleApp/iDiscipleApp/Schedule Tab/ScheduleViewController.swift
@@ -11,131 +11,110 @@ import SwiftyJSON
 
 class ScheduleViewController: ButtonBarPagerTabStripViewController {
 
-    var isReload = false
-    var scheduleView: ScheduleView!
-    
-    //let blueInstagramColor = UIColor(red: 37/255.0, green: 111/255.0, blue: 206/255.0, alpha: 1.0)
-    let day1VC = ScheduleListViewController(itemInfo: "2019-05-21")
-    let day2VC = ScheduleListViewController(itemInfo: "2019-05-22")
-    let day3VC = ScheduleListViewController(itemInfo: "2019-05-23")
-    let day4VC = ScheduleListViewController(itemInfo: "2019-05-24")
-    
-    override func viewDidLoad() {
-        
-        scheduleView = ScheduleView(frame: CGRect.zero)
-        self.view.addSubview(scheduleView)
-        
-        // AutoLayout
-        scheduleView.autoPinEdgesToSuperviewEdges(with: UIEdgeInsets.zero)
-        
-        self.containerView = scheduleView.scrollView
-        self.buttonBarView = scheduleView.buttonBarView
-        
-        settings.style.buttonBarBackgroundColor = .white
-        settings.style.buttonBarItemBackgroundColor = .white
-        settings.style.selectedBarBackgroundColor = .black
-        settings.style.buttonBarItemFont = UIFont(name: "Montserrat-Bold", size: 24)!//.boldSystemFont(ofSize: 14)
-        settings.style.selectedBarHeight = 0.0
-        settings.style.buttonBarMinimumLineSpacing = 0
-        settings.style.buttonBarItemTitleColor = .black
-        settings.style.buttonBarItemsShouldFillAvailableWidth = true
-        settings.style.buttonBarLeftContentInset = 0
-        settings.style.buttonBarRightContentInset = 0
+  var isReload = false
+  var scheduleView: ScheduleView!
 
-        changeCurrentIndexProgressive = {(oldCell: ButtonBarViewCell?, newCell: ButtonBarViewCell?, progressPercentage: CGFloat, changeCurrentIndex: Bool, animated: Bool) -> Void in
-            guard changeCurrentIndex == true else { return }
-            oldCell?.label.textColor = .lightGray
-            newCell?.label.textColor = .black
-        }
-        
-        super.viewDidLoad()
-        
-        // Do any additional setup after loading the view.
+  //let blueInstagramColor = UIColor(red: 37/255.0, green: 111/255.0, blue: 206/255.0, alpha: 1.0)
+  let day1VC = ScheduleListViewController(itemInfo: "2019-12-26")
+  let day2VC = ScheduleListViewController(itemInfo: "2019-12-27")
+  let day3VC = ScheduleListViewController(itemInfo: "2019-12-28")
+
+  override func viewDidLoad() {
+
+    scheduleView = ScheduleView(frame: CGRect.zero)
+    self.view.addSubview(scheduleView)
+
+    // AutoLayout
+    scheduleView.autoPinEdgesToSuperviewEdges(with: UIEdgeInsets.zero)
+
+    self.containerView = scheduleView.scrollView
+    self.buttonBarView = scheduleView.buttonBarView
+
+    settings.style.buttonBarBackgroundColor = .white
+    settings.style.buttonBarItemBackgroundColor = .white
+    settings.style.selectedBarBackgroundColor = .black
+    settings.style.buttonBarItemFont = UIFont(name: "Montserrat-Bold", size: 24)!//.boldSystemFont(ofSize: 14)
+    settings.style.selectedBarHeight = 0.0
+    settings.style.buttonBarMinimumLineSpacing = 0
+    settings.style.buttonBarItemTitleColor = .black
+    settings.style.buttonBarItemsShouldFillAvailableWidth = true
+    settings.style.buttonBarLeftContentInset = 0
+    settings.style.buttonBarRightContentInset = 0
+
+    changeCurrentIndexProgressive = {(oldCell: ButtonBarViewCell?, newCell: ButtonBarViewCell?, progressPercentage: CGFloat, changeCurrentIndex: Bool, animated: Bool) -> Void in
+      guard changeCurrentIndex == true else { return }
+      oldCell?.label.textColor = .lightGray
+      newCell?.label.textColor = .black
     }
-    
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        
-//        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
-//            self.checkDateForTabSelection()
-//        }
-        
-//        DispatchQueue.main.async {
-//            self.checkDateForTabSelection()
-//        }
-        //day1VC.loadSchedule()
-        
+
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+
+  override func viewWillAppear(_ animated: Bool) {
+    super.viewWillAppear(animated)
+
+    self.checkDateForTabSelection()
+  }
+
+  func checkDateForTabSelection(){
+
+    //Check here if workshop/happening or not
+    let stringDay = GlobalConstant.kDateSource_formatter.string(from: Date())
+
+    day1VC.loadSchedule()
+
+    if (stringDay == day1VC.itemInfo.title!){
+      debugPrint("Day 1")
+      moveToViewController(at: 0, animated: true)
+      day1VC.loadSchedule()
     }
-    
-    func checkDateForTabSelection(){
-        
-        //Check here if workshop/happening or not
-        let stringDay = GlobalConstant.kDateSource_formatter.string(from: Date())
-        
-        day1VC.loadSchedule()
-        
-        if (stringDay == day1VC.itemInfo.title!){
-            debugPrint("Day 1")
-            moveToViewController(at: 0, animated: true)
-            day1VC.loadSchedule()
-        }
-        if (stringDay == day2VC.itemInfo.title!){
-            debugPrint("Day 2")
-            moveToViewController(at: 1, animated: true)
-            day2VC.loadSchedule()
-        }
-        if (stringDay == day3VC.itemInfo.title!){
-            debugPrint("Day 3")
-            moveToViewController(at: 2, animated: true)
-            day3VC.loadSchedule()
-        }
-        if (stringDay == day4VC.itemInfo.title!){
-            debugPrint("Day 4")
-            moveToViewController(at: 3, animated: true)
-            day4VC.loadSchedule()
-        }
+    if (stringDay == day2VC.itemInfo.title!){
+      debugPrint("Day 2")
+      moveToViewController(at: 1, animated: true)
+      day2VC.loadSchedule()
     }
-    
-    // MARK: - PagerTabStripDataSource
-    
-    override func viewControllers(for pagerTabStripController: PagerTabStripViewController) -> [UIViewController] {
-        
-//        let child_1 = ScheduleListViewController(itemInfo: "2019-05-21")
-//        let child_2 = ScheduleListViewController(itemInfo: "2019-05-22")
-//        let child_3 = ScheduleListViewController(itemInfo: "2019-05-23")
-//        let child_4 = ScheduleListViewController(itemInfo: "2019-05-24")
-        //return [child_1, child_2, child_3, child_4]
-        
-        guard isReload else {
-            return [day1VC, day2VC, day3VC, day4VC]
-        }
-        
-        var childViewControllers = [day1VC, day2VC, day3VC, day4VC]
-        
-        for index in childViewControllers.indices {
-            let nElements = childViewControllers.count - index
-            let n = (Int(arc4random()) % nElements) + index
-            if n != index {
-                childViewControllers.swapAt(index, n)
-            }
-        }
-        let nItems = 1 + (arc4random() % 8)
-        return Array(childViewControllers.prefix(Int(nItems)))
+    if (stringDay == day3VC.itemInfo.title!){
+      debugPrint("Day 3")
+      moveToViewController(at: 2, animated: true)
+      day3VC.loadSchedule()
     }
-    
-    override func reloadPagerTabStripView() {
-        isReload = true
-        if arc4random() % 2 == 0 {
-            pagerBehaviour = .progressive(skipIntermediateViewControllers: arc4random() % 2 == 0, elasticIndicatorLimit: arc4random() % 2 == 0 )
-        } else {
-            pagerBehaviour = .common(skipIntermediateViewControllers: arc4random() % 2 == 0)
-        }
-        super.reloadPagerTabStripView()
+  }
+
+  // MARK: - PagerTabStripDataSource
+
+  override func viewControllers(for pagerTabStripController: PagerTabStripViewController) -> [UIViewController] {
+    guard isReload else {
+      return [day1VC, day2VC, day3VC]
     }
-    
-    override func configureCell(_ cell: ButtonBarViewCell, indicatorInfo: IndicatorInfo) {
-        super.configureCell(cell, indicatorInfo: indicatorInfo)
-        cell.backgroundColor = .clear
+
+    var childViewControllers = [day1VC, day2VC, day3VC]
+
+    for index in childViewControllers.indices {
+      let nElements = childViewControllers.count - index
+      let n = (Int(arc4random()) % nElements) + index
+      if n != index {
+        childViewControllers.swapAt(index, n)
+      }
     }
-    
+    let nItems = 1 + (arc4random() % 8)
+    return Array(childViewControllers.prefix(Int(nItems)))
+  }
+
+  override func reloadPagerTabStripView() {
+    isReload = true
+    if arc4random() % 2 == 0 {
+      pagerBehaviour = .progressive(skipIntermediateViewControllers: arc4random() % 2 == 0, elasticIndicatorLimit: arc4random() % 2 == 0 )
+    } else {
+      pagerBehaviour = .common(skipIntermediateViewControllers: arc4random() % 2 == 0)
+    }
+    super.reloadPagerTabStripView()
+  }
+
+  override func configureCell(_ cell: ButtonBarViewCell, indicatorInfo: IndicatorInfo) {
+    super.configureCell(cell, indicatorInfo: indicatorInfo)
+    cell.backgroundColor = .clear
+  }
+
 }
